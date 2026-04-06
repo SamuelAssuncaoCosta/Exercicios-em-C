363e2f34f572c95a1c39ac03b4b25f6362b69eaa
Samuel Assunção Costa <samuelcostaassuncao140@gmail.com>
2026-04-06 16:40:45 -0300

    Update Cálculo_eleitoral.c
    Adiciona exercicio em C para calcular a porcentagem de eleitores que votaram em nulo, branco ou válido.

--- Cálculo eleitoral/Cálculo_eleitoral.c
+++ Cálculo eleitoral/Cálculo_eleitoral.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+	int eleitores; 
+	int nulos;     
+	int brancos;
+	int validos;
+	float percentual_nulo;  
+	float percentual_valido;
+	float percentual_branco;
+	
+	printf("Qual o total de eleitores do municipio: ");
+	scanf("%d", &eleitores);
+	printf("Quantidade de eleitores que votaram nulo: ");
+	scanf("%d", &nulos);
+	printf("Quantidade eleitores que votaram em branco: ");
+	scanf("%d", &brancos);
+	printf("Quantidade de eleitores que votaram valido: ");
+	scanf("%d", &validos);
+	percentual_nulo = (float)nulos/eleitores * 100;
+	percentual_valido = (float)validos/eleitores * 100; 
+	percentual_branco = (float)brancos/eleitores * 100;
+	
+	
+	printf("\npercentual de eleitores nulo: %f", percentual_nulo);  
+	printf("\npercentual de eleitores validos: %f", percentual_valido);
+	printf("\npercentual de eleitores que votaram em branco: %f\n", percentual_branco);
+	
+	
+	system("pause");
+	return 0;
+}
