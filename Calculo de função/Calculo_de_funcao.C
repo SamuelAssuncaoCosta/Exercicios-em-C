046623785a31b498872c8fa0388ef51d9c599d8d
Samuel Assunção Costa <samuelcostaassuncao140@gmail.com>
2026-04-07 15:58:37 -0300

    Create Calculo_de_funcao.C

---
+++ Calculo de função/Calculo_de_funcao.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int A;
+	int B;
+	int C;
+	int R;
+	int S;
+	float D;
+	
+	printf("Digite o valor de A: ");
+	scanf("%d", &A);
+	printf("Digite o valor de B: ");
+	scanf("%d", &B);
+	printf("Digite o valor de C: ");
+	scanf("%d", &C);
+	
+	R = (A + B) * (A + B) ;
+	S= (B + C) * (B + C);
+	D = (float)(R+S) / 2;
+	
+	printf("O valor de D eh: %f\n", D);
+	
+	system("pause");
+	return 0;
+}
